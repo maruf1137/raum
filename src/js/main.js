6d078abb67374c8db94bdd5ebd69cce5be6d3247
Maruf Mia <mmaruf1137@gmail.com>
2022-06-08 00:09:56 +0600

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// const tooltip = document.querySelector('.tooltip');
+// const tooltipBtn = document.querySelectorAll('.raum__item .btn');
+// console.log(tooltipBtn);
+
+// tooltipBtn.forEach((btn) => {
+// 	btn.addEventListener('mouseover', function () {
+// 		tooltip.style.opacity = 1;
+// 	});
+// 	btn.addEventListener('mouseout', function () {
+// 		tooltip.style.opacity = 0;
+// 	});
+// });
